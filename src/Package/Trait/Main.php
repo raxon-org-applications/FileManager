fc08e496a5662896fb719f1114a1f64eeabd75e9
remco van der Velde <remco.pc@outlook.com>
2025-02-11 20:00:34 +0100

    make install happen

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -157,15 +157,15 @@ trait Main {
                             'source' => $file->url, //used in TemplateExceptions
                         ];
                         $data = new Data();
-                        if(!array_key_exists('subdomain', $response_frontend['node'])){
-                            $data->set('options.frontend.host', $response_frontend['node']['domain'] . '.' . $response_frontend['node']['extension']);
+                        if(!property_exists($response_frontend['node'],'subdomain')){
+                            $data->set('options.backend.host', $response_frontend['node']->domain . '.' . $response_frontend['node']->extension);
                         } else {
-                            $data->set('options.frontend.host', $response_frontend['node']['subdomain'] . '.' . $response_frontend['node']['domain'] . '.' . $response_frontend['node']['extension']);
+                            $data->set('options.backend.host', $response_frontend['node']->subdomain . '.' . $response_frontend['node']->domain . '.' . $response_frontend['node']->extension);
                         }
-                        if(!array_key_exists('subdomain', $response_backend['node'])){
-                            $data->set('options.backend.host', $response_backend['node']['domain'] . '.' . $response_backend['node']['extension']);
+                        if(!property_exists($response_backend['node'],'subdomain')){
+                            $data->set('options.backend.host', $response_backend['node']->domain . '.' . $response_backend['node']->extension);
                         } else {
-                            $data->set('options.backend.host', $response_backend['node']['subdomain'] . '.' . $response_backend['node']['domain'] . '.' . $response_backend['node']['extension']);
+                            $data->set('options.backend.host', $response_backend['node']->subdomain . '.' . $response_backend['node']->domain . '.' . $response_backend['node']->extension);
                         }
                         $parse = new Parse($object, $data, null, $options);
 
