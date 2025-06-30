564079c1fc1e1b24ba3ca134c77e3e5b6b3d34e9
remco van der Velde <remco.pc@outlook.com>
2025-06-30 19:38:35 +0200

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -151,6 +151,9 @@ trait Main {
                                     }
                                     echo Cli::info('Processing file:') . $file->target . PHP_EOL;
                                     File::write($file->target, Core::object($content->data(), Core::JSON));
+                                    File::permission($object, [
+                                        'target' => $file->target,
+                                    ]);
                                     //imports should be in a json file (class => url/contains)
                                     if(str_contains($file->target, 'System.Route')){
                                         $command = 'app raxon/node object import -class=System.Route -url="' . $file->target . '" -patch';
@@ -167,87 +170,29 @@ trait Main {
                                     $clone_options->set('source', $file->url);
                                     $flags = App::flags($clone);
                                     $parse = new Parse($clone, $data, $flags, $clone_options->data());
-                                    $content = $parse->compile(File::read($file->url), $data);
-                                    ddd($content);
+                                    $content = $parse->compile(File::read($file->url), $data);                                    
                                     if($patch !== null) {
                                         File::delete($file->target);
                                     }
                                     echo Cli::info('Processing file:') . $file->target . PHP_EOL;
                                     File::write($file->target, $content);
+                                    File::permission($object, [
+                                        'target' => $file->target,
+                                    ]);
                                 break;
                             }
-                        }                
+                        }                                    
                     }
-                }
-                
-                ddd($file);
-
-
-
-
-                if(!File::exist($file->target) || $patch !== null){
-                    $file->extension = File::extension($file->target);
-                    if($file->extension === 'rax'){
-                        $explode = explode('.rax', $file->target, 2);
-                        if(array_key_exists(1, $explode)){
-                            $file->target = $explode[0];
-                        }
-                        $data = $object->data();
-                        $clone_options = new Data();
-                        if(!property_exists($response_frontend['node'],'subdomain')){
-                            $clone_options->set('frontend.host', $response_frontend['node']->domain . '.' . $response_frontend['node']->extension);
-                        } else {
-                            $clone_options->set('frontend.host', $response_frontend['node']->subdomain . '.' . $response_frontend['node']->domain . '.' . $response_frontend['node']->extension);
-                        }
-                        if(!property_exists($response_backend['node'],'subdomain')){
-                            $clone_options->set('backend.host', $response_backend['node']->domain . '.' . $response_backend['node']->extension);
-                        } else {
-                            $clone_options->set('backend.host', $response_backend['node']->subdomain . '.' . $response_backend['node']->domain . '.' . $response_backend['node']->extension);
-                        }                       
-                        $flags =                          
-                        $parse = new Parse($object, $data);
-                        $file->original_extension = File::extension($file->target);
-                        switch($file->original_extension){
-                            case 'json':
-                                $clone->data($data);
-                                $content = $clone->parse_read($file->url);
-                                if($patch !== null) {
-                                    File::delete($file->target);
-                                }
-                                echo Cli::info('Processing file:') . $file->target . PHP_EOL;
-                                File::write($file->target, Core::object($content->data(), Core::JSON));
-                                //imports should be in a json file (class => url/contains)
-                                if(str_contains($file->target, 'System.Route')){
-                                    $command = 'app raxon/node object import -class=System.Route -url="' . $file->target . '" -patch';
-                                    Core::execute($object, $command, $output, $notification);
-                                    if($output){
-                                        echo $output;
-                                    }
-                                    if($notification){
-                                        echo $notification;
-                                    }
-                                }
-                            break;
-                            default:
-                                $content = $parse->compile(File::read($file->url), $data);
-                                if($patch !== null) {
-                                    File::delete($file->target);
-                                }
-                                echo Cli::info('Processing file:') . $file->target . PHP_EOL;
-                                File::write($file->target, $content);
-                            break;
-                        }
-                    } else {
-                        if($patch !== null) {
-                            File::delete($file->target);
-                        }
-                        echo Cli::debug('Copying file:') . $file->target . PHP_EOL;
-                        File::copy($file->url, $file->target);
+                } else {
+                    if($patch !== null) {
+                        File::delete($file->target);
                     }
+                    echo Cli::info('Processing file:') . $file->target . PHP_EOL;
+                    File::copy($file->url, $file->target);                    
                     File::permission($object, [
                         'target' => $file->target,
                     ]);
-                }
+                }                
             }
         }
     }
